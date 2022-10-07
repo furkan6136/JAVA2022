a7188f314ba4119bf1a5c4683e092096b031f2d1
furkan6136 <furkan_oguzhan_61@hotmail.com>
2022-10-07 19:51:15 +0300

    week3

--- classesWithAttributes/src/classesWithAttributes/Main.java
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -1,10 +1,17 @@
 package classesWithAttributes;
-
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+//https://www.youtube.com/watch?v=EoXqaSX9nrc&list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=32
+    public static void main(String[] args) {
+    	
+        Product product=new Product(1,"Laptop","Asus Laptop",5000,3,"blue");
+        product.setId(1);
+        product.setName("Laptop");
+        product.setDescription("Asus Laptop");
+        product.setPrice(5000);
+        product.setStockAmount(3);
+        product.setRenk("");
+        ProductManager productManager=new ProductManager();
+        productManager.add(product);
+        System.out.println(product.getKod());
+    }
 }
